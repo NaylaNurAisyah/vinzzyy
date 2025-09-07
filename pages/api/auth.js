faf1a89da606b10ecaabe42ce8dcb1243ddee9c4
NaylaNurAisyah <98145666+NaylaNurAisyah@users.noreply.github.com>
2025-09-07 14:23:08 +0700

    Update auth.js

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") return res.status(200).end();
+  res.status(200).json({ message: "API jalan di Vercel 👍" });
 
   await client.connect();
   const usersCollection = client.db(dbName).collection("users");
