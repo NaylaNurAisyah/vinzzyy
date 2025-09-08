ee48c24e0f7caa3744bf01864273fbc5c69907cc
NaylaNurAisyah <98145666+NaylaNurAisyah@users.noreply.github.com>
2025-09-08 17:34:00 +0700

    Update users.js

--- api/users.js
+++ api/users.js
@@ -5,10 +5,11 @@ const client = new MongoClient(uri);
 const dbName = 'Database_Vinzzyy';
 
 export default async function handler(req, res) {
-  // Tambah header CORS untuk semua jenis request
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  // CORS headers
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*"); 
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   // Handle preflight request
   if (req.method === 'OPTIONS') {
